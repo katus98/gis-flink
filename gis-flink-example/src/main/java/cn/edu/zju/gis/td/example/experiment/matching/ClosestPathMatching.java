0bd7a9465b059537843412e0428114f340bb9878
katus <sunkeran0818@outlook.com>
2022-12-24 15:11:33 +0800

    修正最近路径匹配算法中对匹配状态的更新

--- gis-flink-example/src/main/java/cn/edu/zju/gis/td/example/experiment/matching/ClosestPathMatching.java
+++ gis-flink-example/src/main/java/cn/edu/zju/gis/td/example/experiment/matching/ClosestPathMatching.java
@@ -72,6 +72,7 @@ public class ClosestPathMatching extends RichMapFunction<GpsPoint, MatchingResul
                 mr = candidate;
             }
         }
+        matchingResultState.update(mr);
         return mr;
     }
 
